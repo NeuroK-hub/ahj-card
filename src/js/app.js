68b9e6bdfb5b9a9b47d722d6a810b0c0c20ce72f
NeuroK-hub <102793426+NeuroK-hub@users.noreply.github.com>
2023-10-26 18:56:23 +0300

    1

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+// TODO: write code here
+
+// // comment this to pass build
+// // const unusedVariable = 'variable';
+
+// // for demonstration purpose only
+// export default function demo(value) {
+//   return value;
+// }
+
+// console.log('app.js included');
+
+import CardFormValidator from './widget';
+
+const container = document.querySelector('.container');
+const form = new CardFormValidator(container);
+
+form.bindToDom();
